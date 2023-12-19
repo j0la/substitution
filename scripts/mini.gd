b25b7b9519d3f136ef57cfe2353e19aad53e8a5c
Jonathan Olaleye <69396515+j0la@users.noreply.github.com>
2023-12-19 01:43:36 -0600

    added world interpolation

--- scripts/mini.gd
+++ scripts/mini.gd
@@ -1,20 +1,24 @@
 extends Node
 
-# Mini is designed to work with the Substitution movement provider
-# when an avatar is spawned, the configured world tree is scaled down and attached to the configured controller
-# the other controller may then grab and manipulate the avatar in the mini world before performing a substitution
-# using a mini world enables more extreme avatar manipulation like substituting over long distances or through walls
-# scaling up and down are triggered through signals
+# Mini minifies a world tree to enable more dramatic interaction (e.g. moving objects long distances and through walls)
+# it's designed to work with the Substitution movement provider for greater avatar manipulation
+# the avatar_spawn and start_sub signals should be connected to scale_down and scale_up
+# after scaling down, the mini world is attached to the configured "hold controller"
+# the "manipulate controller" may then translate and rotate the mini world while interacting with the scene
 
+@export var player: XRToolsPlayerBody
 @export var hold_controller: XRController3D
 @export var manipulate_controller: XRController3D
-@export var world: Node3D # world tree to minify
-@export var mini_scale: float = 0.1
-@export var translate_speed: float = 3.0
-@export var rotate_speed: float = 3.0
+@export var world: Node3D
+@export var mini_scale := 0.05
+@export var interpolation_speed := 2.0
+@export var translate_speed := 3.0
+@export var rotate_speed := 3.0
+@export var enabled := true
 
 var world_in_hand := false
-var deadzone = 0.1
+var target_world_xform: Transform3D
+var lerp_t := 0.0
 
 @onready var world_parent: Node3D = world.get_parent()
 
@@ -25,27 +29,39 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
+	if !enabled: return
+	
+	if lerp_t < 1:
+		world.transform = world.transform.interpolate_with(target_world_xform, lerp_t)
+		lerp_t += delta * interpolation_speed
+		return
+	elif !world_in_hand and !player.enabled:
+		# world is back to normal scale, unfreeze
+		player.set_enabled(true)
+		
 	if world_in_hand:
+		# manipulate mini world
+		# translation and rotation done one at a time
 		var input = manipulate_controller.get_vector2('primary')
-		if abs(input[1]) > deadzone:
+		if abs(input[1]) > abs(input[0]):
 			world.transform = world.transform.translated(Vector3(0, 0, -input[1] * translate_speed * delta))
-		if abs(input[0]) > deadzone:
+		else:
 			world.global_transform = world.global_transform.rotated_local(Vector3(0, 1, 0), input[0] * rotate_speed * delta)
 	
-func scale_down(player_body: XRToolsPlayerBody):
+func scale_down():
+	if !enabled: return
 	# freeze player movement
-	player_body.set_enabled(false)
+	player.set_enabled(false)
 	# minify world & attach to controller
-	world.set_scale(Vector3(mini_scale, mini_scale, mini_scale))
 	world.reparent(hold_controller)
 	world_in_hand = true
-	world.set_position(Vector3(0, 0, 0))
-	world.set_rotation_degrees(Vector3(0, 0, 0))
+	target_world_xform = Transform3D().scaled(Vector3(mini_scale, mini_scale, mini_scale))
+	lerp_t = 0
 	
-func scale_up(player_body: XRToolsPlayerBody):
+func scale_up():
+	if !enabled: return
 	# restore world
 	world.reparent(world_parent)
 	world_in_hand = false
-	world.set_identity()
-	# unfreeze player
-	player_body.set_enabled(true)
+	target_world_xform = Transform3D()
+	lerp_t = 0
--- scripts/substitution.gd
+++ scripts/substitution.gd
@@ -6,16 +6,18 @@ extends XRToolsMovementProvider
 # provides teleportation with rapid transition via linear interpolation
 # pressing the configured button drops an avatar at the player's position
 # pressing the button a second time teleports the player to the avatar
+# a controller's button_pressed signal should be attached to _on_button_pressed to trigger the mechanic
 
-signal avatar_spawn(player_body: XRToolsPlayerBody) # emitted when player drops an avatar
-signal start_sub(player_body: XRToolsPlayerBody) # emitted when player requests to substitute avatar
-signal end_sub(player_body: XRToolsPlayerBody) # emitted when player replaces avatar
+signal avatar_spawn() # emitted when player drops an avatar
+signal start_sub() # emitted when player begins substitution (start of interpolation)
+signal end_sub() # emitted when player replaces avatar (end of interpolation)
 
-@export var order: int = 1 # movement provider priority (lowest runs first)
-@export var button: String = "trigger_click"
-@export var speed: float = 20 # interpolation speed while entering & exiting avatar
-@export var avatar: Node3D # avatar to interpolate towards
-@export var avatar_offset: float = 0 # distance from avatar to stop interpolation
+@export var button := "trigger_click"
+@export var avatar: Node3D # object to spawn and substitute
+@export var avatar_offset := 0 # distance from avatar to stop interpolation
+@export var interpolation_speed := 20 # avatar interpolation speed
+
+var order := 1 # movement provider priority (lowest runs first)
 
 @onready var _player := XRHelpers.get_xr_origin(self).get_node('PlayerBody')
 
@@ -43,28 +45,31 @@ func physics_movement(delta: float, player_body: XRToolsPlayerBody, disabled: bo
 			player_body.velocity = player_body.move_body(Vector3(0, 0, 0))
 			is_active = false
 			avatar.visible = false
-			end_sub.emit(player_body)
+			end_sub.emit()
 		else:
 			# interpolate towards avatar
 			var lerp_dir := player_body.global_position.direction_to(avatar.global_position)
-			player_body.velocity = player_body.move_body(lerp_dir * speed)
+			player_body.velocity = player_body.move_body(lerp_dir * interpolation_speed)
 		
 		# return true to indicate to player body that this was an exclusive movement
 		# gravity & ground physics wont be applied
 		return true
 
 func _on_button_pressed(name: String):
+	if !enabled or !avatar:
+		return
+		
 	if name == button:
 		if avatar.visible:
 			# avatar is present, activate lerp
 			if !is_active:
-				start_sub.emit(_player)
+				start_sub.emit()
 				is_active = true
 		else:
 			# drop avatar at player position
 			avatar.global_transform = _player.global_transform
 			avatar.visible = true
-			avatar_spawn.emit(_player)
+			avatar_spawn.emit()
 
 # verify the movement provider has a valid configuration.
 func _get_configuration_warnings() -> PackedStringArray:
